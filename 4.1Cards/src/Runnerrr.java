63c08dcce8928f49447436c57d008b0a362ef2c7
AneudysGermosen <32362463+AneudysGermosen@users.noreply.github.com>
2018-03-20 09:19:03 -0400

    Update Runnerrr.java

--- 4.1Cards/src/Runnerrr.java
+++ 4.1Cards/src/Runnerrr.java
@@ -5,7 +5,7 @@ public class Runnerrr {
 
 			String[] rank = { "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace", "2" };
 			String[] suits = { "Hearts", "Clubs", "Spades", "Diamonds" };
-			int[] pointValue = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+			int[] value = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
 
 			Deck d = new Deck(rank, suits, pointValue);
 			d.shuffle();
